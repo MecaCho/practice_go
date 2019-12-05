19f4ecd2ef2ed998a7db013b1319f84b081fc7a8
MecaCho <1099833328@qq.com>
2019-12-05 23:29:39 +0800

    add go pointer test;

---
+++ gopoint/go_pointer.go
@@ -0,0 +1,37 @@
+package gopoint
+
+import "fmt"
+
+type Group struct {
+	Name string
+	ID int64
+}
+
+type User struct {
+	Name string
+	Group *Group
+	Group1 Group
+}
+
+func Pointer()  {
+	var user User
+	user.Name = "test_user"
+	var group Group
+	user.Group = &group
+	ChangePointer(user)
+	ChangeGroup(user)
+	fmt.Printf("user : %+v, group: %+v, group1: %+v.", user, user.Group, user.Group1)
+}
+
+func ChangePointer(user User)  {
+	user.Group.Name = "test_name"
+	user.Group.ID = int64(1)
+}
+
+func ChangeGroup(user User)  {
+	user.Group1.Name = "test_name"
+	user.Group1.ID = int64(1)
+}
+
+
+
---
+++ gopoint/go_pointer_test.go
@@ -0,0 +1,7 @@
+package gopoint
+
+import "testing"
+
+func TestPointer(t *testing.T) {
+	Pointer()
+}
