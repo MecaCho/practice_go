fa44a19c9c531319ba15e5bf11a791b3002e77d6
MecaCho <1099833328@qq.com>
2020-07-06 00:19:51 +0800

    add server;

---
+++ server1.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+"fmt"
+"log"
+"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// w.Write([]byte(r.URL.Path))
+	fmt.Fprintf(w, "Get url PATH: %s\n", r.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8001", nil))
+}
