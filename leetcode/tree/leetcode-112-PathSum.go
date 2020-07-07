e6d08f3538774acf978c18415b447c5d9dfe0588
MecaCho <1099833328@qq.com>
2020-07-07 23:52:56 +0800

    add golang echo;

--- leetcode/tree/leetcode-112-PathSum.go
+++ leetcode/tree/leetcode-112-PathSum.go
@@ -1,7 +1,5 @@
 package tree
 
-
-
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -13,17 +11,16 @@ package tree
 
 func hasPathSum(root *TreeNode, sum int) bool {
 
-	if root == nil{
+	if root == nil {
 		return false
 	}
-	if root.Left == nil && root.Right == nil && root.Val == sum{
+	if root.Left == nil && root.Right == nil && root.Val == sum {
 		return true
 	}
 
 	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
 
 }
-
 
 // 112. 路径总和
 // 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
