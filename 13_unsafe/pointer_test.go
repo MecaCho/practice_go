d5e5726b3917ee35eef055a187f6980415986dd8
MecaCho <1099833328@qq.com>
2020-07-24 00:04:02 +0800

    add unsafe pointer;

---
+++ 13_unsafe/pointer_test.go
@@ -0,0 +1,21 @@
+package _3_unsafe
+
+import (
+	"testing"
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+func TestBytesToString(t *testing.T) {
+	s := "abcdefg"
+	res := StringToBytes(s)
+	stringHeader := (*reflect.StringHeader)(unsafe.Pointer(&s))
+
+	fmt.Printf("%+v, %+v\n", &s, &stringHeader.Data)
+	fmt.Println(res)
+
+	bytes := []byte(s)
+	res1 := BytesToString(bytes)
+	fmt.Println(res1)
+}
