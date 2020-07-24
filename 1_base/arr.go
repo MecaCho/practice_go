c2bdf978ef9fef58ad9081d186608e05a790e3c3
MecaCho <1099833328@qq.com>
2020-07-24 23:22:27 +0800

    add unsafe package test;

--- 1_base/arr.go
+++ 1_base/arr.go
@@ -1,11 +1,30 @@
 package __base
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+	)
 
 func Arr() {
-
+	c := make(chan int)
+	close(c)
+	close(c)
 	arr := [3]int{1, 2, 3}
 	arr0 := [...]int{1, 2, 3}
 
 	fmt.Println(arr, arr0)
+}
+
+func Matrix()  {
+	fmt.Println("Size of int: ", unsafe.Sizeof(int(0)))
+	matirx := [2][3]int{[3]int{1,2,3}, [3]int{4,5,6}}
+
+	for i, v := range matirx{
+
+		for j, vv := range v{
+			fmt.Println(vv)
+			fmt.Println(i, j, matirx[i][j], &matirx[i][j])
+		}
+	}
+
 }
--- 1_base/arr_test.go
+++ 1_base/arr_test.go
@@ -5,3 +5,7 @@ import "testing"
 func TestArr(t *testing.T) {
 	Arr()
 }
+
+func TestMatrix(t *testing.T) {
+	Matrix()
+}
