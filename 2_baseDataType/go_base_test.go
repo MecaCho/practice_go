fe2629f2285eb0850589cc854bcc6dea6f2b3739
MecaCho <1099833328@qq.com>
2020-09-30 22:45:58 +0800

    add golang slice test;

--- 2_baseDataType/go_base_test.go
+++ 2_baseDataType/go_base_test.go
@@ -1,6 +1,9 @@
 package __baseDataType
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestDeferTest(t *testing.T) {
 	// DeferTest()
@@ -14,4 +17,17 @@ func TestCapOfSlice(t *testing.T) {
 
 func TestSliceTest(t *testing.T) {
 	SliceTest()
+}
+
+func TestDeferTest2(t *testing.T) {
+	DeferTest()
+}
+
+func TestSliceTest2(t *testing.T) {
+	s := make([]int, 10)
+	fmt.Println(len(s), cap(s), s)
+	for i := 0;i< 10; i++{
+		s = append(s, i)
+	}
+	fmt.Println(s)
 }
