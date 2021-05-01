e31022be9848ee247f942358e200124ae4ec08f4
MecaCho <1099833328@qq.com>
2021-05-01 23:24:00 +0800

    Create 690_test.go
    add 690. Employee Importance test;

---
+++ leetcode/dis/690_test.go
@@ -0,0 +1,14 @@
+package dfs
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetImportance(t *testing.T) {
+	res := GetImportance([]*Employee{
+		{1, 2, []int{2}},
+		{2, 3, []int{}},
+	}, 2)
+	fmt.Println(res)
+}
